016f52b8097c90a4374824b15daffcd8a8b4d264
Michał Suliborski <msuliborski@gmail.com>
2019-10-27 20:49:07 +0100

    fill data function

--- Project1/Data/Reader.cs
+++ Project1/Data/Reader.cs
@@ -4,10 +4,11 @@ using System.Text;
 
 namespace Data {
     public class Reader {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
 
         public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Book> Books { get; set; }
 
         public Reader(int id, string firstName, string lastName) {
             Id = id;
