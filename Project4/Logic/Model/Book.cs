331370d257ccf2a5ee9f2245421411b427fe563b
ninjarlz <michal.kusmidrowicz@gmail.com>
2020-01-16 22:06:02 +0100

    crud ext 2

--- Project4/Logic/Model/Book.cs
+++ Project4/Logic/Model/Book.cs
@@ -2,12 +2,20 @@
 
 namespace Services.Model {
     public class Book {
-        public int? CatalogId;
+        public int? CatalogId { get; set; }
         public int IdNumber { get; set; }
+        public int? ReaderId { get; set; }
 
         public Book(int? catalogId, int idNumber) {
             CatalogId = catalogId;
             IdNumber = idNumber;
+            ReaderId = null;
+        }
+
+        public Book(int? catalogId, int? readerId, int idNumber) {
+            CatalogId = catalogId;
+            IdNumber = idNumber;
+            ReaderId = readerId;
         }
     }
 }
